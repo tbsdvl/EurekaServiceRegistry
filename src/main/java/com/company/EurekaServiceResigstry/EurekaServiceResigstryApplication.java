a88497af0130e26ac06a80079adef3685f80fabf
Triston Burns <tristonburns97@gmail.com>
2022-03-10 15:06:55 -0700

    init commit

---
+++ src/main/java/com/company/EurekaServiceResigstry/EurekaServiceResigstryApplication.java
@@ -0,0 +1,13 @@
+package com.company.EurekaServiceResigstry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceResigstryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceResigstryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/EurekaServiceResigstry/EurekaServiceResigstryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.EurekaServiceResigstry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceResigstryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
